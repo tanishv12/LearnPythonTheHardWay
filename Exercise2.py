a657ebdb46511ce4edf2c3d79678d0c4c240c97c
Tanish Visanagiri <65863468+tanishv12@users.noreply.github.com>
2021-06-07 23:42:05 +0530

    Comments and Pound Characters
    Exercise-2 of LPTHW.

---
+++ Exercise2.py
@@ -0,0 +1,11 @@
+#CommentsAndPoundCharacters   I use this to identify which program is in use.
+
+#A comment, this is so you can read your program later.
+#Anything after # is ignored by Python.
+
+print("I could have code like this.")# and the comment after is ignored
+
+#You can also comment to remove some part of code like 
+#print("This won't run.")
+
+print("This will run.")
