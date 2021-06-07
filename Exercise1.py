743facc59d9822d47d23e49715cd559feec24d61
Tanish Visanagiri <65863468+tanishv12@users.noreply.github.com>
2021-06-07 23:35:18 +0530

    A Good First Program.
    Exercise-1 of LPTHW.

---
+++ Exercise1.py
@@ -0,0 +1,8 @@
+#AGoodFirstProgram    This text is hidden from the output.
+print("Hello World!")
+print("Hello again")
+print("I like typing this.")
+print("This is fun.")
+print("I'd much rather you 'not'.")
+print("Yay! Printing.")
+print('I "said" do not touch this.')
