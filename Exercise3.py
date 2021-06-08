299ef23611d0482aeb451266ace3f8c34578d186
Tanish Visanagiri <65863468+tanishv12@users.noreply.github.com>
2021-06-08 18:47:41 +0530

    Numbers and Math.
    Exercise-3 of LPTHW.

---
+++ Exercise3.py
@@ -0,0 +1,17 @@
+#NumbersAndMath
+print("I will now count my chickens:")
+print("Hens -", 25+30/6)
+print("Roosters -", 100-25*3%4) #% solves for the remainder
+
+print("Now I will count the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7) #prints true or false(relational operator?) prints false because 5>-2
+print("What is 3+2?", 3+2)
+print("What is 5-7?", 5-7)
+print("Oh, that's why its False.")
+
+print("How about some more.")
+print("Is it greater?",5>-2) #true
+print("Is it greater or equal?", 5<=-2) #false
