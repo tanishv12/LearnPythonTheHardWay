4c8cc98e921e28263447c3f7543ae3b9e14951b5
Tanish Visanagiri <65863468+tanishv12@users.noreply.github.com>
2021-06-08 18:54:24 +0530

    More Variables and Printing.
    Exercise-5 of LPTHW.

---
+++ Exercise5.py
@@ -0,0 +1,18 @@
+#MoreVariablesAndPrinting
+my_name = 'Tanish Visanagiri'
+my_age = 15
+my_height = 70 
+my_weight = 135
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If i add {my_age}, {my_height} and {my_weight} I get {total}.")
