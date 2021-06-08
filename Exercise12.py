1817493fbb53a1a280d54fd6ea6a3ef9e5a6adc9
Tanish Visanagiri <65863468+tanishv12@users.noreply.github.com>
2021-06-08 19:08:28 +0530

    Prompting People.
    Exercise-12 of LPTHW.

---
+++ Exercise12.py
@@ -0,0 +1,6 @@
+#PromptingPeople
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So, you're {age} old, {height} tall, {weight} heavy.")
