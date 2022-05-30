63c21a31a96b115a76e6a71ad2e1da1b52f4b66c
Tanish Visanagiri <tanishv12@gmail.com>
2022-05-30 11:21:01 +0530

    Exercises

---
+++ Exercise38.py
@@ -0,0 +1,24 @@
+#Doings Things to Lists
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Dog"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do something with stuff.")
+
+print(stuff[1])
+print(stuff[-1])# woah! fancy
+print(stuff.pop())
+print(' '.join(stuff))# what? cool!
+print('#'.join(stuff[3:5]))# super stellar!
